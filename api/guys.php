cf033dd20e18c7f9b27ba22f5005df6d94c196f2
brou <brousse@openmailbox.org>
2016-01-07 17:09:15 +0100

    commented solution in api/guys.php

--- api/guys.php
+++ api/guys.php
@@ -3,6 +3,13 @@
 // Commenting this will make the preflight fail.
 header("Access-Control-Allow-Headers: authorization");
 
+// Something like this will also fail:
+// header("Access-Control-Allow-Headers: example");
+
+// Whereas something like that works:
+// header("Access-Control-Allow-Headers: example, authorization");
+
+header('Access-Control-Allow-Methods: GET,  OPTIONS');
 header("Access-Control-Allow-Origin: http://localhost:4200");
 header("Access-Control-Allow-Credentials: true");
 
